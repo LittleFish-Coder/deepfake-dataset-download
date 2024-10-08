d6c45ee3295c8ed26e37f9465f5474608bb36161
LittleFish-Coder <adrianyu890317@gmail.com>
2024-10-09 01:11:50 +0800

    add notice

--- WildDeepfake/download_WildDeepFake.py
+++ WildDeepfake/download_WildDeepFake.py
@@ -4,3 +4,5 @@ import gdown
 gdrive_folder_link = 'https://drive.google.com/drive/folders/1Cb_OqksBU3x7HFIo8EvDTigU6IjM7tmp'
 
 gdown.download_folder(gdrive_folder_link, quiet=True)
+
+# if error occurs, try to download the dataset manually from the link above
