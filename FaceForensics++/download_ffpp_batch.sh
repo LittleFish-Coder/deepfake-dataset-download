d5800418fd8a72d208b06d9d74a83c967b6ab8b4
LittleFish-Coder <adrianyu890317@gmail.com>
2024-10-15 01:37:01 +0800

    update compression

--- FaceForensics++/download_ffpp_batch.sh
+++ FaceForensics++/download_ffpp_batch.sh
@@ -5,6 +5,11 @@ ALL_DATASETS=("original" "DeepFakeDetection_original" "Deepfakes"
               "DeepFakeDetection" "Face2Face" "FaceShifter" "FaceSwap" 
               "NeuralTextures")
 
+# Compression levels for the videos
+# c23(hq original sequence images/videos)
+# c40(lq original sequence images/videos)
+COMPRESSION=("c23", "c40")
+
 # Run the faceforensics_download_v4.py script with the specified parameters
 
 ## Download 'original' dataset
@@ -13,19 +18,19 @@ echo "" | python faceforensics_download_v4.py ./ -d original -c c23 -t videos --
 
 ## Download 'Deepfakes' dataset
 echo "Downloading 'Deepfakes' dataset"
-echo "" | python faceforensics_download_v4.py ./ -d Deepfakes -c c40 -t videos --server EU2
+echo "" | python faceforensics_download_v4.py ./ -d Deepfakes -c c23 -t videos --server EU2
 
 ## Download 'Face2Face' dataset
 echo "Downloading 'Face2Face' dataset"
-echo "" | python faceforensics_download_v4.py ./ -d Face2Face -c c40 -t videos --server EU2
+echo "" | python faceforensics_download_v4.py ./ -d Face2Face -c c23 -t videos --server EU2
 
 ## Download 'FaceSwap' dataset
 echo "Downloading 'FaceSwap' dataset"
-echo "" | python faceforensics_download_v4.py ./ -d FaceSwap -c c40 -t videos --server EU2
+echo "" | python faceforensics_download_v4.py ./ -d FaceSwap -c c23 -t videos --server EU2
 
 ## Download 'NeuralTextures' dataset
 echo "Downloading 'NeuralTextures' dataset"
-echo "" | python faceforensics_download_v4.py ./ -d NeuralTextures -c c40 -t videos --server EU2
+echo "" | python faceforensics_download_v4.py ./ -d NeuralTextures -c c23 -t videos --server EU2
 
 # ================================================================================================= #
 # Below datasets are not mentioned in the FF++ Dataset paper, but are available for download
@@ -35,12 +40,12 @@ echo "" | python faceforensics_download_v4.py ./ -d NeuralTextures -c c40 -t vid
 
 ## Download 'DeepFakeDetection_original' dataset
 # echo "Downloading 'DeepFakeDetection_original' dataset"
-# echo "" | python faceforensics_download_v4.py ./ -d DeepFakeDetection_original -c c40 -t videos --server EU2
+# echo "" | python faceforensics_download_v4.py ./ -d DeepFakeDetection_original -c c23 -t videos --server EU2
 
 ## Download 'DeepFakeDetection' dataset
 # echo "Downloading 'DeepFakeDetection' dataset"
-# echo "" | python faceforensics_download_v4.py ./ -d DeepFakeDetection -c c40 -t videos --server EU2
+# echo "" | python faceforensics_download_v4.py ./ -d DeepFakeDetection -c c23 -t videos --server EU2
 
 ## Download 'FaceShifter' dataset
 # echo "Downloading 'FaceShifter' dataset"
-# echo "" | python faceforensics_download_v4.py ./ -d FaceShifter -c c40 -t videos --server EU2
+# echo "" | python faceforensics_download_v4.py ./ -d FaceShifter -c c23 -t videos --server EU2
